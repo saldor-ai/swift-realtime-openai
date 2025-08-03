1755f015884b15ce87b97ebe9b2a406d91e22468
Jacob Bland <jacobrbland@gmail.com>
2025-08-03 11:05:32 -0700

    make webrtc endpoints public

--- src/OpenAIRealtime.swift
+++ src/OpenAIRealtime.swift
@@ -50,12 +50,12 @@ extension RealtimeAPI {
 	}
 
 	/// Connect to the OpenAI WebRTC Realtime API with the given request.
-	static func webRTC(connectingTo request: URLRequest) async throws -> RealtimeAPI {
+	public static func webRTC(connectingTo request: URLRequest) async throws -> RealtimeAPI {
 		try RealtimeAPI(connector: await WebRTCConnector(connectingTo: request))
 	}
 
 	/// Connect to the OpenAI WebRTC Realtime API with the given authentication token and model.
-	static func webRTC(authToken: String, model: String = "gpt-4o-realtime-preview") async throws -> RealtimeAPI {
+	public static func webRTC(authToken: String, model: String = "gpt-4o-realtime-preview") async throws -> RealtimeAPI {
 		var request = URLRequest(url: URL(string: "wss://api.openai.com/v1/realtime")!.appending(queryItems: [
 			URLQueryItem(name: "model", value: model),
 		]))
